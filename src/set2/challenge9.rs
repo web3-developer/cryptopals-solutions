62b3d2e2460bf462c4bf4cec20eb107d0099e78c
Web3 Developer <51288821+web3-developer@users.noreply.github.com>
2023-07-05 18:59:02 +0800

    Completed challenge 9.

--- src/set2/challenge9.rs
+++ src/set2/challenge9.rs
@@ -1,14 +1,35 @@
 
 
+pub fn pad_using_pkcs7(block_size: u8, input: &[u8]) -> Vec<u8> {
+    if block_size <= 0 || input.len() <= 0 {
+        panic!("block_size and input must be greater than 0");
+    }
+
+    let remainder = block_size - (input.len() % block_size as usize) as u8;
+
+    let padding = match remainder {
+        0 => vec![block_size; block_size as usize],
+        _ => vec![remainder; remainder as usize]
+    };
+
+    [input, &padding].concat()
+}
 
 #[cfg(test)]
 mod tests {
 
-    //use super::*;
+    use super::*;
+
 
     #[test]
     fn run_challenge9() {
+        let block_size: u8 = 20;
+        let input = "YELLOW SUBMARINE".as_bytes();
+        let expected = "YELLOW SUBMARINE\x04\x04\x04\x04".as_bytes();
 
+        let result = pad_using_pkcs7(block_size, &input);
+
+        assert_eq!(expected, result)
     }
 
 }
